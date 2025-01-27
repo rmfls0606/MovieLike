51b4d744d1b35106847f3d8ea980054138b07458
rmfls0606 <rmfls0606@naver.com>
2025-01-27 23:39:19 +0900

    프로필 이미지 설정 뷰에서 보여줄 이미지들을 CollectionView로 보여주기 위해 BaseCollectionViewCell 파일 생성

---
+++ MovieLike/Profile/View/ProfileSelectImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileSelectImageCollectionViewCell.swift
+//  MovieLike
+//
+//  Created by 이상민 on 1/27/25.
+//
+
+import UIKit
+
+class ProfileSelectImageCollectionViewCell: UICollectionViewCell {
+    
+}
