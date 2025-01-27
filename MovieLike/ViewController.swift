e6e6a3a32bfdd7f6d579a17bf25dcf26e517986c
rmfls0606 <rmfls0606@naver.com>
2025-01-27 19:31:51 +0900

    리소스 파일 생성

---
+++ MovieLike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLike
+//
+//  Created by 이상민 on 1/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
