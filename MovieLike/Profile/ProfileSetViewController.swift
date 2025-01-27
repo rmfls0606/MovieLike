c32b5ea8b515e713056b7f7ddad3dcae644e0a99
rmfls0606 <rmfls0606@naver.com>
2025-01-27 19:52:08 +0900

    Profile폴더 생성 및 View, ViewController파일 생성

---
+++ MovieLike/Profile/ProfileSetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileSetViewController.swift
+//  MovieLike
+//
+//  Created by 이상민 on 1/27/25.
+//
+
+import UIKit
+
+final class ProfileSetViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    private func configure(){ }
+}
---
+++ MovieLike/Profile/View/ProfileView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileView.swift
+//  MovieLike
+//
+//  Created by 이상민 on 1/27/25.
+//
+
+import UIKit
+
+final class ProfileView: BaseView {
+    
+}
