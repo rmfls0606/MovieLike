6895e07eb459d098a1ecf1832d102f1a769aed6b
rmfls0606 <rmfls0606@naver.com>
2025-02-12 04:32:59 +0900

    [리팩토링] SearchResultViewModel 파일 생성

---
+++ MovieLike/SearchResult/ViewModel/SearchResultViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  SearchResultViewModel.swift
+//  MovieLike
+//
+//  Created by 이상민 on 2/12/25.
+//
+
+import Foundation
+
+class SearchResultViewModel: BaseViewModel {
+    private(set) var input: Input
+    private(set) var output: Output
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+
+    }
+    
+    init () {
+        input = Input()
+        output = Output()
+        
+        transform()
+    }
+    
+    func transform() {
+     
+    }
+}
