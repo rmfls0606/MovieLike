7179c13b108acd8147fe1e0f10ff3a253127aca0
rmfls0606 <rmfls0606@naver.com>
2025-02-12 17:18:53 +0900

    MovieDetailViewModel 파일 생성

---
+++ MovieLike/MovieDetail/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailViewModel.swift
+//  MovieLike
+//
+//  Created by 이상민 on 2/12/25.
+//
+
+import Foundation
+
+class MovieDetailViewModel: BaseViewModel{
+    private(set) var input: Input
+    private(set) var output: Output
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    init () {
+        input = Input()
+        output = Output()
+        
+        transform()
+    }
+    
+    func transform() {
+        
+    }
+}
